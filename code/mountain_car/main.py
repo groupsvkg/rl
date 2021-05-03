dc5646c2d9437d45f4e4e4ffd9ff8c70467f72c9
Vishal Kumar Gupta <groupsvkg@gmail.com>
2021-05-03 14:22:33 +0100

    ReplayMemory class done

--- code/mountain_car/main.py
+++ code/mountain_car/main.py
@@ -1,35 +1,38 @@
 import gym
 
 
-class MountainCar:
-    def __init__(self, env, episodes=100, max_episode_length=1000,
-                 replay_memory_capacity=int(1e6), replay_minibatch_size=32,
-                 actor_learning_rate=1e-4, critic_learning_rate=1e-3, actor_noise=0.1,
-                 discount_factor=0.99, soft_target_update_factor=0.001):
-        self.env = env
-        self.episodes = episodes
-        self.max_episode = max_episode_length
-        self.replay_memory_capacity = replay_memory_capacity
-        self.replay_minibatch_size = replay_minibatch_size
-        self.actor_learning_rate = actor_learning_rate
-        self.critic_learning_rate = critic_learning_rate
-        self.actor_noise = actor_noise
-        self.discount_factor = discount_factor
-        self.soft_target_update_factor = soft_target_update_factor
+# class MountainCar:
+#     def __init__(self, env, episodes=100, max_episode_length=1000,
+#                  replay_memory_capacity=int(1e6), replay_minibatch_size=32,
+#                  actor_learning_rate=1e-4, critic_learning_rate=1e-3, actor_noise=0.1,
+#                  discount_factor=0.99, soft_target_update_factor=0.001):
+#         self.env = env
+#         self.episodes = episodes
+#         self.max_episode = max_episode_length
+#         self.replay_memory_capacity = replay_memory_capacity
+#         self.replay_minibatch_size = replay_minibatch_size
+#         self.actor_learning_rate = actor_learning_rate
+#         self.critic_learning_rate = critic_learning_rate
+#         self.actor_noise = actor_noise
+#         self.discount_factor = discount_factor
+#         self.soft_target_update_factor = soft_target_update_factor
 
-    def apply_ddgp(self):
+#     def apply_ddgp(self):
+#         pass
 
-        # import gym
-        # env = gym.make('MountainCarContinuous-v0')
-        # for i_episode in range(2):
-        #     observation = env.reset()
-        #     for t in range(100):
-        #         env.render()
-        #         action = env.action_space.sample()
-        #         observation, reward, done, info = env.step(action)
-        #         print(i_episode, t, observation, action, reward, done, info)
+#  import gym
+env = gym.make('MountainCarContinuous-v0')
+print("State Space", *env.observation_space.shape)
+print("Action Space", env.action_space.shape)
+# for i_episode in range(2):
+#     observation = env.reset()
+#     for t in range(100):
+#         env.render()
+#         action = env.action_space.sample()
+#         observation, reward, done, info = env.step(action)
+#         print(i_episode, t, observation, action, reward, done, info)
 
-        #         if done:
-        #             print("Episode finished after {} timesteps".format(t+1))
-        #             break
-        # env.close()
+#         if done:
+#             print("Episode finished after {} timesteps".format(t+1))
+#             break
+# env.close()
--- code/mountain_car/replay_memory.py
+++ code/mountain_car/replay_memory.py
@@ -1,9 +1,41 @@
-class ReplayMemory:
-    def __init__(self):
-        pass
+import numpy as np
+import torch
 
-    def store(self):
-        pass
+
+class ReplayMemory:
+    def __init__(self, env, replay_memory_capacity, replay_minibatch_size):
+        self.env = env
+        self.replay_memory_capacity = replay_memory_capacity
+        self.replay_minibatch_size = replay_minibatch_size
+
+        self.current_states = np.zeros(
+            (replay_memory_capacity, *env.observation_space.shape), dtype=np.float32)
+        self.next_states = np.zeros(
+            (replay_memory_capacity, *env.observation_space.shape), dtype=np.float32)
+        self.actions = np.zeros(
+            (replay_memory_capacity, env.action_space.shape[0]), dtype=np.float32)
+        self.rewards = np.zeros(replay_memory_capacity, dtype=np.float32)
+        self.dones = np.zeros(replay_memory_capacity, dtype=np.float32)
+        self.rollback_pointer = 0
+        self.sample_interval = 0
+
+    def store(self, current_state, action, reward, next_state, done):
+        self.current_states[self.rollback_pointer] = current_state
+        self.actions[self.rollback_pointer] = action
+        self.rewards[self.rollback_pointer] = reward
+        self.next_states[self.rollback_pointer] = next_state
+        self.dones[self.rollback_pointer] = done
+        self.rollback_pointer = (
+            self.rollback_pointer + 1) % self.replay_memory_capacity
+        self.sample_interval = min(
+            self.sample_interval+1, self.replay_memory_capacity)
 
     def sample(self):
-        pass
+        idxs = np.random.randint(
+            0, self.sample_interval, size=self.replay_minibatch_size)
+        batch = dict(current_states=self.current_states[idxs],
+                     actions=self.actions[idxs],
+                     rewards=self.rewards[idxs],
+                     next_states=self.next_states[idxs],
+                     dones=self.dones[idxs])
+        return {k: torch.as_tensor(v, dtype=torch.float32) for k, v in batch.items()}
